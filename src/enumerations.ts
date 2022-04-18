2b899df656edbe140b6b5d0d649741e002e13148
Hsan Muhamad Jaballi <hassan.benmouhamad@gmail.com>
2022-04-18 03:54:02 +0200

    init: enumerations.ts

---
+++ src/enumerations.ts
@@ -0,0 +1,13 @@
+export enum SUPPORTED_CONTENT_TYPES {
+    JSON = "json",
+    RAW = "text",
+    DEFAULT = "blob",
+}
+
+export enum SPEC_HEADER_KEYS {
+    CONTENT_TYPE = "content-type",
+}
+
+export enum FALLBACK_VALUES {
+    CONTENT_TYPE = "default",
+}
